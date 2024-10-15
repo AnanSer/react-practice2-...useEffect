66d4269a4f463c91aacf9fb9593e1af3bb2f8841
AnanSer <ananserbesa@gmail.com>
2024-10-15 15:12:01 +0300

    added useEffect cleanup function

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,18 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      console.log("checking the password");
+
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log("clean up");
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
